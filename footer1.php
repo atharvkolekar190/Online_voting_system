34ede1fb331147c9bf5594d7a603d2e5f24513fc
Atharv Kolekar <kolekar.atharv1812@gmail.com>
2025-03-10 09:49:55 +0530

    New Only Voting Management System for College

---
+++ footer1.php
@@ -0,0 +1,8 @@
+<hr>
+
+<footer>
+  <div class="foot_center2">
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Copyright &copy; 2025 PCCOE</p>
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Programmed by: AK190</p>
+  </div>
+</footer>
